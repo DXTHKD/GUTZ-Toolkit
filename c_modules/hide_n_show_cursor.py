a9c2d17822246eed2ddfc5ecdb9388472531332f
MXRDA <139921262+DXTHKD@users.noreply.github.com>
2023-08-31 20:44:02 +0200

    Add files via upload

---
+++ c_modules/hide_n_show_cursor.py
@@ -0,0 +1,12 @@
+import os
+import time as dfhgdfhsfjrsdnewcrecwoigvmuivnweravzbuwvenwrrwebrnizumrtvreecbnznunuwedfhgdfhsfjrsdnewcrecwoigvmuivnweravzbuwvenwrrwebrnizumrtvreecbnznunuwe
+
+def hidecursor():
+    os.system("<nul set /p=[?25l")
+def showcursor():
+    os.system("<nul set /p=[?25h")
+    
+if __name__ == "__main__":
+    print(" [-] This is a Module, not a Script to run.")
+    dfhgdfhsfjrsdnewcrecwoigvmuivnweravzbuwvenwrrwebrnizumrtvreecbnznunuwedfhgdfhsfjrsdnewcrecwoigvmuivnweravzbuwvenwrrwebrnizumrtvreecbnznunuwe.sleep(3)
+    exit()
