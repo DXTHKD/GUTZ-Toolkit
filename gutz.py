c1c66a613d28c3dc00d61ed50a09cd302dde421a
YUNGRVT <139921262+yungrvt@users.noreply.github.com>
2023-09-01 16:45:24 +0200

    Add files via upload

--- gutz.py
+++ gutz.py
@@ -58,6 +58,15 @@ GITHUB_USER = "yungrvt"
 GITHUB_REPO = "GUTZ-Toolkit"
 GITHUB_TOKEN = os.environ.get("GIIITHUB_TOKEN")
 
+user32 = ctypes.WinDLL('user32')
+
+MB_OK = 0x00000000
+MB_ICONERROR = 0x00000010
+MB_ICONINFORMATION = 0x00000040
+MB_ICONWARNING = 0x00000030
+MB_ICONQUESTION = 0x00000020
+MB_YESNO = 0x00000004
+
 def get_latest_release_version():
     url = f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/releases/latest"
     response = requests.get(url)
@@ -65,54 +74,58 @@ def get_latest_release_version():
     latest_version = data["tag_name"]
     return latest_version
 
-def update_project():
-    latest_version = get_latest_release_version()
-
-    if latest_version == CURRENT_VERSION:
-        print("aight")
-        ctypes.windll.user32.MessageBoxW(0, "No update available.", "Update Check", 0)
-    else:
-        result = ctypes.windll.user32.MessageBoxW(0, f"Theres a new version of GUTZ available ({latest_version}). \n\nDo you want to update?", f"GUTZ {CURRENT_VERSION} - Update", 4)
+latest_version = get_latest_release_version()
+if latest_version == CURRENT_VERSION:
+    pass
+else:
+    result = ctypes.windll.user32.MessageBoxW(0, f"Theres a new version of GUTZ available ({latest_version}). \n\nDo you want to update?", f"GUTZ {CURRENT_VERSION} - Update", 4)
+    
+    if result == 6:
+        zip_url = f"https://github.com/{GITHUB_USER}/{GITHUB_REPO}/releases/download/{latest_version}/gutz-toolkit.zip"
+        zip_filename = f"gutz-toolkit.zip"
+        temp_folder = "temp_extracted_folder"
         
-        if result == 6:  # Yes button was clicked
-            zip_url = f"https://github.com/{GITHUB_USER}/{GITHUB_REPO}/releases/download/{latest_version}/gutz-toolkit.zip"
-            zip_filename = f"gutz-toolkit.zip"
-            temp_folder = "temp_extracted_folder"
+        subprocess.run(["curl", "-LO", zip_url])
+        
+        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+            zip_ref.extractall(temp_folder)
             
-            subprocess.run(["curl", "-LO", zip_url])
-            
-            with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-                zip_ref.extractall(temp_folder)
+        time.sleep(2)
+        
+        for item in os.listdir("."):
+            if os.path.isdir(item) and item != temp_folder:
+                shutil.rmtree(item)
+            elif os.path.isfile(item) and item != "autoupdate.py":
+                os.remove(item)
                 
-            time.sleep(2)
-            
-            for item in os.listdir("."):
-                if os.path.isdir(item) and item != temp_folder:
-                    shutil.rmtree(item)
-                elif os.path.isfile(item) and item != "autoupdate.py":
-                    os.remove(item)
-                    
-            time.sleep(2)
-            
-            for item in os.listdir(temp_folder):
-                if item != "autoupdate.py":
-                    item_path = os.path.join(temp_folder, item)
-                    shutil.move(item_path, ".")
-                    
-            time.sleep(2)
-            
-            shutil.rmtree(temp_folder)
-            
-            time.sleep(3)
-            
-            ctypes.windll.user32.MessageBoxW(0, f"Succesfully updated!", f"GUTZ", MB_OK)
-
-            
-update_project()
+        time.sleep(2)
+        
+        for item in os.listdir(temp_folder):
+            if item != "autoupdate.py":
+                item_path = os.path.join(temp_folder, item)
+                shutil.move(item_path, ".")
+                
+        time.sleep(2)
+        
+        shutil.rmtree(temp_folder)
+        
+        time.sleep(3)
+        
+        ctypes.windll.user32.MessageBoxW(0, f"Succesfully updated!", f"GUTZ", MB_OK)
+        
+        exit()
+    
+    else:
+        pass
 
 ###########
 # <setup> #
 ###########
+
+if latest_version == CURRENT_VERSION:
+    pass
+else:
+    ctypes.windll.user32.MessageBoxW(0, f"Warning, you are currently running a outdated version.", f"GUTZ", MB_OK)
 
 script_dirrr = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dirrr)
@@ -188,15 +201,6 @@ def delete_webhook(webhook_id):
         print(Colorate.Color(Colors.red, " Done.", True))
     else:
         print(" Invalid webhook ID.")
-    
-user32 = ctypes.WinDLL('user32')
-
-MB_OK = 0x00000000
-MB_ICONERROR = 0x00000010
-MB_ICONINFORMATION = 0x00000040
-MB_ICONWARNING = 0x00000030
-MB_ICONQUESTION = 0x00000020
-MB_YESNO = 0x00000004
 
 def cmd_size(columns, lines):
   command1 = f"mode con cols={columns} lines={lines}"
@@ -286,7 +290,9 @@ def login_tui():
   if str(ping_time) == "None":
     cmd_title(f"GUTZ V1.0 | error_checking_ping")
   def login(username, password):
-    if username == "root" and password == "root":
+    kaya = os.environ.get("KAYA")
+    print(kaya)
+    if username == "kaya" and password == kaya:
       logging.info(f"{pcuser} logged in as {username} with password: {password}")
       main_menu()
     else:
